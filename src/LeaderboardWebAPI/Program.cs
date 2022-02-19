abae59602547be34f35f096d0e447f05ac4f7cd9
Alex Thissen <athissen@xpirit.com>
2022-02-19 19:23:52 +0100

    End of lab 8

--- src/LeaderboardWebAPI/Program.cs
+++ src/LeaderboardWebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +21,19 @@ namespace LeaderboardWebAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("secrets/appsettings.secrets.json", optional: true);
+                    var hostConfig = config.Build();
+                    if (!String.IsNullOrEmpty(hostConfig["KeyVaultName"]))
+                    {
+                        var secretClient = new SecretClient(
+                            new Uri(hostConfig["KeyVaultName"]),
+                            new DefaultAzureCredential()
+                        );
+                        config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
